c2def94d059a738af5891fc89b6e210a6b2f5eea
mker <krmichael25@gmail.com>
2025-03-11 20:58:29 +0600

    Upload new recipe to API, show it in the view

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 8;
 export const RES_PER_PAGE = 10;
+export const KEY = '0de1ee55-4bae-4a92-b530-9da085555238';
+export const MODAL_CLOSE_SEC = 2.5;
