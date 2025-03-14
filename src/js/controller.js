0b39c318d9717b2f922c1453bd68c93663aaf5a1
mker <krmichael25@gmail.com>
2025-03-14 20:34:14 +0600

    Clean up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,11 +10,6 @@ import addRecipeView from './views/addRecipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-// Parcel hot reload
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -73,7 +68,6 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
 
